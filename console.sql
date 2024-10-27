9445d0fbd6ba76a6e807ab0db31d9e1fc8a4a104
IrwinJuice <sevriukovmk@gmail.com>
2024-10-27 12:38:56 +0200

    feat: add get_parents proc

--- console.sql
+++ console.sql
@@ -1,3 +1,18 @@
+select *
+from user_objects
+where object_name = 'testProc2';
+select *
+from user_objects
+where object_name = 'MyType';
+select *
+from all_errors
+where upper(name) = upper('testProc2');
+select *
+from all_errors
+where upper(name) = upper('MyType');
+
+
+
 CREATE TYPE t_tf_row AS OBJECT
 (
     id          NUMBER,
@@ -42,6 +57,18 @@ create table DEV_TABLE
     NAME VARCHAR2(20 CHAR)
 );
 
+create table DEV_TABLE_REL
+(
+    IDI        number,
+    IDI_PARENT number
+);
+
+create table DEV_TABLE_SYNC
+(
+    idi  number,
+    sync varchar2(1 char) default 'N'
+);
+
 -- TRUNCATE TABLE PROD_TABLE;
 TRUNCATE TABLE DEV_TABLE;
 
@@ -58,6 +85,27 @@ insert into DEV_TABLE (IDI, NAME)
 VALUES (2, 'Hellen_DEV');
 insert into DEV_TABLE (IDI, NAME)
 VALUES (3, 'Irwin');
+insert into DEV_TABLE (IDI, NAME)
+VALUES (5, 'Minion');
+
+
+insert into DEV_TABLE_REL(IDI, IDI_PARENT)
+values (2, 1);
+insert into DEV_TABLE_REL(IDI, IDI_PARENT)
+values (3, 1);
+insert into DEV_TABLE_REL(IDI, IDI_PARENT)
+values (5, 3);
+
+insert into DEV_TABLE_SYNC
+values (1, 'N');
+insert into DEV_TABLE_SYNC
+values (2, 'N');
+insert into DEV_TABLE_SYNC
+values (3, 'N');
+insert into DEV_TABLE_SYNC
+values (5, 'N');
+
+
 
 create or replace PROCEDURE testProc
 AS
@@ -83,11 +131,6 @@ BEGIN
         VALUES (tmp.idi, tmp.name);
 END testProc;
 
-select *
-from PROD_TABLE;
-select *
-from DEV_TABLE;
-
 DECLARE
 
 BEGIN
@@ -101,41 +144,69 @@ create global temporary table tmp_id_table
 )
     on commit delete rows;
 
-create or replace PROCEDURE testProc2(idi_list in varchar2) AS
+
+
+-- select r.idi_parent as idi, r.l
+--     from DEV_TABLE_SYNC s, (
+--         select IDI_PARENT, level l from DEV_TABLE_REL
+--                                    start with idi = ?
+--                                    connect by idi = prior IDI_PARENT and IDI_PARENT != prior idi) r
+--         where r.IDI_PARENT = s.idi and s.sync = 'N'
+--             order by r.l desc;
+
+create or replace procedure get_parents(id in number) AS
 BEGIN
+
+    insert into tmp_id_table
+    select r.idi_parent as idi
+    from DEV_TABLE_SYNC s,
+         (select IDI_PARENT, level l
+          from DEV_TABLE_REL
+          start with idi = id
+          connect by idi = prior IDI_PARENT
+                 and IDI_PARENT != prior idi) r
+    where r.IDI_PARENT = s.idi
+      and s.sync = 'N'
+    order by r.l desc;
+
+
+END get_parents;
+
+create or replace PROCEDURE testProc2(idi_list in varchar2) AS
+    cou integer;
+BEGIN
+
     insert into tmp_id_table
     select to_number(trim(n)) id
     from (SELECT REGEXP_SUBSTR(idi_list, '[^,]+', 1, level) n
           FROM dual
           CONNECT BY REGEXP_SUBSTR(idi_list, '[^,]+', 1, level) IS NOT NULL);
 
+    select count(*) into cou from tmp_id_table;
+
+    for row in (select * from tmp_id_table)
+        loop
+            get_parents(row.id);
+        end loop;
+
 
     update DEV_TABLE
     set name='TEST_NAME'
     where idi in (select * from tmp_id_table);
 
-    --     update DEV_TABLE
---     set name='TEST_NAME'
---     where idi in (SELECT to_number(REGEXP_SUBSTR(idi_list, '[^,]+', 1, level))
---                   FROM dual
---                   CONNECT BY REGEXP_SUBSTR(idi_list, '[^,]+', 1, level) IS NOT NULL);
 END testProc2;
+
 
 DECLARE
 BEGIN
-    testProc2('1,2,3');
+    testProc2('5');
 END;
 /
 
 select *
-from user_objects
-where object_name = 'testProc2';
+from PROD_TABLE;
 select *
-from user_objects
-where object_name = 'MyType';
+from DEV_TABLE;
 select *
-from all_errors
-where upper(name) = upper('testProc2');
-select *
-from all_errors
-where upper(name) = upper('MyType');
+from DEV_TABLE_REL;
+
